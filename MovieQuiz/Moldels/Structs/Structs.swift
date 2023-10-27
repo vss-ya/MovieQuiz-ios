3577bbe15a772fd762351db50a41abfe9d937ab4
vs <vss.direct.g@yandex.ru>
2023-10-27 14:31:43 +0700

    check_1

---
+++ MovieQuiz/Moldels/Structs/Structs.swift
@@ -0,0 +1,18 @@
+//
+//  Structs.swift
+//  MovieQuiz
+//
+//  Created by vs on 27.10.2023.
+//
+
+import Foundation
+
+struct QuizQuestion {
+  // строка с названием фильма,
+  // совпадает с названием картинки афиши фильма в Assets
+  let image: String
+  // строка с вопросом о рейтинге фильма
+  let text: String
+  // булевое значение (true, false), правильный ответ на вопрос
+  let correctAnswer: Bool
+} 
